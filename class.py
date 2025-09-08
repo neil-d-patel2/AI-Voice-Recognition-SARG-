7f739afc9964def4336403fcb82e3bc06be516b7
Neil <ebox381@gmail.com>
2025-09-07 22:07:09 -0400

    Testing langchain

--- class.py
+++ class.py
@@ -1,22 +1,30 @@
 import whisper
 
 # Load model (base is a good starting point)
-model = whisper.load_model("base")
 
-# Transcribe audio
-result = model.transcribe("output.mp3", fp16=False)
-print("Transcription:", result["text"])
+def transcribe_audio(file_path: str) -> str:
 
+    model = whisper.load_model("base")
 
-"""
-Write each "Chunked text into a txt file for parsing"
-"""
-
-parse = result["text"]
-with open("game_transcript.txt","w") as f:
-    f.write(parse)
-    f.write('\n')
+    # Transcribe audio
+    result = model.transcribe("output.mp3", fp16=False)
 
 
+    """
+    Write each "Chunked text into a txt file for parsing"
+    """
+
+    parse = result["text"]
+    with open("game_transcript.txt","w") as f:
+        f.write(parse)
+        f.write('\n')
+
+    return result["text"]
 
 
+if __name__ == "__main__":
+    text = transcribe_audio("output.mp3")
+    print("Announcer said")
+    print(text)
+
+    
